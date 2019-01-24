605d0f5b5f2a63911fc4c89f1510101c8e27d3de
Hunter <hunteramerritt1997@gmail.com>
2019-01-23 22:45:21 -0600

    First

---
+++ starkdevelop.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+@app.route("/elle")
+def elle():
+    return render_template("elle.html")
+
+@app.route("/hannah")
+def hannah():
+    return render_template("hannah.html")
+
+@app.route("/kate")
+def kate():
+    return render_template("kate.html")
+
+
+
+
+if __name__ == '__main__':
+    app.run()
